edeb9d788cbcb4c4f9ae4cc55e7acf08c29386e9
Ryan Sage <ryansage09@gmail.com>
2019-08-10 14:57:56 -0400

    added hearts/clubs suit to deck

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //deck array
 let cardImgDir = "css/cardPNG/";
 let deck = [
+    //suit diamonds
     {cardVar: '2',
     cardSuit: 'diamonds',
     cardVal: 2,
@@ -34,13 +35,13 @@ let deck = [
     {cardVar: '8',
     cardSuit: 'diamonds',
     cardVal: 8,
-    carImg: cardImgDir+"2_of_diamonds.png"
+    carImg: cardImgDir+"8_of_diamonds.png"
     },
     {cardVar: '9',
     cardSuit: 'diamonds',
     cardVal: 9,
-    carImg: cardImgDir+"2_of_diamonds.png"
-    },
+    carImg: cardImgDir+"9_of_diamonds.png"
+    }
     {cardVar: '10',
     cardSuit: 'diamonds',
     cardVal: 10,
@@ -59,13 +60,145 @@ let deck = [
     {cardVar: 'King',
     cardSuit: 'diamonds',
     cardVal: 10,
-    carImg: cardImgDir+"King_of_diamonds.png"
+    carImg: cardImgDir+"king_of_diamonds.png"
     },
     {cardVar: 'Ace',
     cardSuit: 'diamonds',
     cardVal: 11,
-    carImg: cardImgDir+"Ace_of_diamonds.png"
-    }
+    carImg: cardImgDir+"ace_of_diamonds.png"
+    },
+    //suit hearts
+    {cardVar: '2',
+    cardSuit: 'hearts',
+    cardVal: 2,
+    carImg: cardImgDir+"2_of_hearts.png"
+    },
+    {cardVar: '3',
+    cardSuit: 'hearts',
+    cardVal: 3,
+    carImg: cardImgDir+"3_of_hearts.png"
+    },
+    {cardVar: '4',
+    cardSuit: 'hearts',
+    cardVal: 4,
+    carImg: cardImgDir+"4_of_hearts.png"
+    },
+    {cardVar: '5',
+    cardSuit: 'hearts',
+    cardVal: 5,
+    carImg: cardImgDir+"5_of_hearts.png"
+    },
+    {cardVar: '6',
+    cardSuit: 'hearts',
+    cardVal: 6,
+    carImg: cardImgDir+"6_of_hearts.png"
+    },
+    {cardVar: '7',
+    cardSuit: 'hearts',
+    cardVal: 7,
+    carImg: cardImgDir+"7_of_hearts.png"
+    },
+    {cardVar: '8',
+    cardSuit: 'hearts',
+    cardVal: 8,
+    carImg: cardImgDir+"2_of_hearts.png"
+    },
+    {cardVar: '2',
+    cardSuit: 'hearts',
+    cardVal: 9,
+    carImg: cardImgDir+"2_of_hearts.png"
+    },
+    {cardVar: '10',
+    cardSuit: 'hearts',
+    cardVal: 10,
+    carImg: cardImgDir+"10_of_hearts.png"
+    },
+    {cardVar: 'Jack',
+    cardSuit: 'diamonds',
+    cardVal: 10,
+    carImg: cardImgDir+"jack_of_hearts.png"
+    },
+    {cardVar: 'Queen',
+    cardSuit: 'hearts',
+    cardVal: 10,
+    carImg: cardImgDir+"queen_of_hearts.png"
+    },
+    {cardVar: 'King',
+    cardSuit: 'hearts',
+    cardVal: 10,
+    carImg: cardImgDir+"king_of_hearts.png"
+    },
+    {cardVar: 'Ace',
+    cardSuit: 'hearts',
+    cardVal: 11,
+    carImg: cardImgDir+"ace_of_hearts.png"
+    },
+      //suit clubs
+      {cardVar: '2',
+      cardSuit: 'clubs',
+      cardVal: 2,
+      carImg: cardImgDir+"2_of_clubs.png"
+      },
+      {cardVar: '3',
+      cardSuit: 'clubs',
+      cardVal: 3,
+      carImg: cardImgDir+"3_of_clubs.png"
+      },
+      {cardVar: '4',
+      cardSuit: 'clubs',
+      cardVal: 4,
+      carImg: cardImgDir+"4_of_clubs.png"
+      },
+      {cardVar: '5',
+      cardSuit: 'clubs',
+      cardVal: 5,
+      carImg: cardImgDir+"5_of_clubs.png"
+      },
+      {cardVar: '6',
+      cardSuit: 'clubs',
+      cardVal: 6,
+      carImg: cardImgDir+"6_of_clubs.png"
+      },
+      {cardVar: '7',
+      cardSuit: 'clubs',
+      cardVal: 7,
+      carImg: cardImgDir+"7_of_clubs.png"
+      },
+      {cardVar: '8',
+      cardSuit: 'clubs',
+      cardVal: 8,
+      carImg: cardImgDir+"2_of_clubs.png"
+      },
+      {cardVar: '2',
+      cardSuit: 'clubs',
+      cardVal: 9,
+      carImg: cardImgDir+"2_of_clubs.png"
+      },
+      {cardVar: '10',
+      cardSuit: 'clubs',
+      cardVal: 10,
+      carImg: cardImgDir+"10_of_clubs.png"
+      },
+      {cardVar: 'Jack',
+      cardSuit: 'diamonds',
+      cardVal: 10,
+      carImg: cardImgDir+"jack_of_clubs.png"
+      },
+      {cardVar: 'Queen',
+      cardSuit: 'clubs',
+      cardVal: 10,
+      carImg: cardImgDir+"queen_of_clubs.png"
+      },
+      {cardVar: 'King',
+      cardSuit: 'clubs',
+      cardVal: 10,
+      carImg: cardImgDir+"king_of_clubs.png"
+      },
+      {cardVar: 'Ace',
+      cardSuit: 'clubs',
+      cardVal: 11,
+      carImg: cardImgDir+"ace_of_clubs.png"
+      },
 ]
  //deck card variables   
 let number = ['2', '3', '4' ,'5', '6', '7', '8', '9', '10']
@@ -75,7 +208,7 @@ let suit = ['diamond', 'heart', 'diamond', 'heart']
 //global vars and arrays
 let playerName = "" 
 let shuffledDeck = []
-let cardPlayed = (shuffledDeck[i].cardVar+" of "+shuffledDeck[i].cardSuit) //calls cardPlayed for message prompts
+//let cardPlayed = (shuffledDeck[i].cardVar+" of "+shuffledDeck[i].cardSuit) //calls cardPlayed for message prompts
 
 
 
