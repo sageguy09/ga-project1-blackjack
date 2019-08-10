6a58784b7303abaa2ebcc8fc95ee04c84a384caa
Ryan Sage <ryansage09@gmail.com>
2019-08-10 14:40:42 -0400

    Completed diamond suited cards. added cardPlayed var. Updated cardImg extension in card properties

--- js/main.js
+++ js/main.js
@@ -4,30 +4,70 @@ let deck = [
     {cardVar: '2',
     cardSuit: 'diamonds',
     cardVal: 2,
-    carImg: cardImgDir+"2_of_diamonds"
+    carImg: cardImgDir+"2_of_diamonds.png"
     },
     {cardVar: '3',
     cardSuit: 'diamonds',
     cardVal: 3,
-    carImg: cardImgDir+"3_of_diamonds"
+    carImg: cardImgDir+"3_of_diamonds.png"
     },
     {cardVar: '4',
     cardSuit: 'diamonds',
     cardVal: 4,
-    carImg: cardImgDir+"4_of_diamonds"
+    carImg: cardImgDir+"4_of_diamonds.png"
     },
     {cardVar: '5',
     cardSuit: 'diamonds',
     cardVal: 5,
-    carImg: cardImgDir+"5_of_diamonds"
+    carImg: cardImgDir+"5_of_diamonds.png"
     },
     {cardVar: '6',
     cardSuit: 'diamonds',
     cardVal: 6,
-    carImg: cardImgDir+"6_of_diamonds"
+    carImg: cardImgDir+"6_of_diamonds.png"
     },
+    {cardVar: '7',
+    cardSuit: 'diamonds',
+    cardVal: 7,
+    carImg: cardImgDir+"7_of_diamonds.png"
+    },
+    {cardVar: '8',
+    cardSuit: 'diamonds',
+    cardVal: 8,
+    carImg: cardImgDir+"2_of_diamonds.png"
+    },
+    {cardVar: '9',
+    cardSuit: 'diamonds',
+    cardVal: 9,
+    carImg: cardImgDir+"2_of_diamonds.png"
+    },
+    {cardVar: '10',
+    cardSuit: 'diamonds',
+    cardVal: 10,
+    carImg: cardImgDir+"10_of_diamonds.png"
+    },
+    {cardVar: 'Jack',
+    cardSuit: 'diamonds',
+    cardVal: 10,
+    carImg: cardImgDir+"jack_of_diamonds.png"
+    },
+    {cardVar: 'Queen',
+    cardSuit: 'diamonds',
+    cardVal: 10,
+    carImg: cardImgDir+"queen_of_diamonds.png"
+    },
+    {cardVar: 'King',
+    cardSuit: 'diamonds',
+    cardVal: 10,
+    carImg: cardImgDir+"King_of_diamonds.png"
+    },
+    {cardVar: 'Ace',
+    cardSuit: 'diamonds',
+    cardVal: 11,
+    carImg: cardImgDir+"Ace_of_diamonds.png"
+    }
 ]
-    
+ //deck card variables   
 let number = ['2', '3', '4' ,'5', '6', '7', '8', '9', '10']
 let face = ['king', 'queen', 'jack', 'ace']
 let suit = ['diamond', 'heart', 'diamond', 'heart']
@@ -35,6 +75,7 @@ let suit = ['diamond', 'heart', 'diamond', 'heart']
 //global vars and arrays
 let playerName = "" 
 let shuffledDeck = []
+let cardPlayed = (shuffledDeck[i].cardVar+" of "+shuffledDeck[i].cardSuit) //calls cardPlayed for message prompts
 
 
 
