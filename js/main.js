ba6f00416e75957707c42c1cb91eda3d2dfd947f
Ryan Sage <ryansage09@gmail.com>
2019-08-10 00:10:49 -0400

    updated wireframe variables/colors. started onStartPrompt functions

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//global
+let playerName = "";
+
+$(".onStartPrompt").submit(function(event){
+    playerName = $('.onPlayerName')
+    console.log(playerName);
+event.preventDefault();
+    //$('.PlayerName').val = $('.onPlayerName') 
+
+$(".onSubmitBtn")
+})
