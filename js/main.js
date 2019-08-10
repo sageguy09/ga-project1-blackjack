9ca03507513e8c21346246abe3cf8925a0806e5e
Ryan Sage <ryansage09@gmail.com>
2019-08-10 14:28:24 -0400

    added cards to deck

--- js/main.js
+++ js/main.js
@@ -6,7 +6,26 @@ let deck = [
     cardVal: 2,
     carImg: cardImgDir+"2_of_diamonds"
     },
-
+    {cardVar: '3',
+    cardSuit: 'diamonds',
+    cardVal: 3,
+    carImg: cardImgDir+"3_of_diamonds"
+    },
+    {cardVar: '4',
+    cardSuit: 'diamonds',
+    cardVal: 4,
+    carImg: cardImgDir+"4_of_diamonds"
+    },
+    {cardVar: '5',
+    cardSuit: 'diamonds',
+    cardVal: 5,
+    carImg: cardImgDir+"5_of_diamonds"
+    },
+    {cardVar: '6',
+    cardSuit: 'diamonds',
+    cardVal: 6,
+    carImg: cardImgDir+"6_of_diamonds"
+    },
 ]
     
 let number = ['2', '3', '4' ,'5', '6', '7', '8', '9', '10']
@@ -15,6 +34,7 @@ let suit = ['diamond', 'heart', 'diamond', 'heart']
 
 //global vars and arrays
 let playerName = "" 
+let shuffledDeck = []
 
 
 
@@ -27,7 +47,7 @@ $(".onStartPrompt").submit(function(event){
     else {
         playerName = "Player"
     }
-    console.log(playerName);
+    console.log(playerName); //remove after debugging
     event.preventDefault();
 })
 
